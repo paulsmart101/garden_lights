0bd4937d98bf0951e4cdc8c36dfeb24b5b7b9e99
paulsmart101 <paulsmart101@gmail.com>
2016-09-08 19:56:51 +0100

    First commit

---
+++ switch_off.sh
@@ -0,0 +1,1 @@
+python /home/pi/garden_lights/socket_off.py 2>&1 >>/home/pi/garden_lights/garden.log
---
+++ switch_on.sh
@@ -0,0 +1,1 @@
+python /home/pi/garden_lights/socket_on.py 2>&1 >>/home/pi/garden_lights/garden.log
